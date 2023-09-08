c210f1d16084f5e3548feb6321f8ded0f6b4d65d
Agrima5115 <90694817+Agrima5115@users.noreply.github.com>
2023-09-08 22:23:22 +0530

    Login Form

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,90 +1,103 @@
-package main.example.dice_roll
+package main.example.loginform
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DiceRollTheme {
-                DiceRollerApp()
+            Column(modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                simpleText()
+                simpleForm()
+                Column (modifier = Modifier.fillMaxSize(1f) ,verticalArrangement = Arrangement.Bottom, horizontalAlignment = Alignment.CenterHorizontally) {
+                    simpleButton()
+                }
             }
         }
     }
 }
+
+@Composable
+fun simpleText(){
+
+    Text(text = "Login Form", modifier = Modifier.padding(16.dp),
+        style = TextStyle(fontSize = 42.sp)
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun simpleTextField(){
+
+    var text by remember { mutableStateOf("") }
+    TextField(value = text , onValueChange = {it -> text = it }, label = { Text(text = "User Name")}  )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun simpleTextFieldp(){
+
+    var text by remember { mutableStateOf("") }
+    TextField(value = text , onValueChange = {it -> text = it }, label = { Text(text = "Password")}  )
+}
+
+@Composable
+fun simpleForm() {
+    Column {
+        simpleTextField()
+        Spacer(modifier = Modifier.height(8.dp)) // Adding space
+        simpleTextFieldp()
+    }
+}
+
+@Composable
+fun simpleButton(){
+
+    Button(modifier = Modifier.padding(16.dp), onClick = { /*TODO*/ }) {
+        Text(text = "LOGIN")
+    }
+
+
+}
+
 
 @Preview
 @Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage()
-}
+fun simpleTextPreview(){
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        simpleText()
 
-@Composable
-fun DiceRollTheme(content: @Composable () -> Unit) {
-    MaterialTheme(
-        content = content
-    )
-}
-
-@Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier
-    .fillMaxSize()
-    .wrapContentSize(Alignment.Center)
-) {
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when (result)
-    {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "YOU ROLLED: $result",
-            modifier = Modifier.padding(8.dp) 
-        )
-        Image(
-            painter = painterResource(id = imageResource),
-            contentDescription = result.toString()
-        )
+        simpleForm()
         Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = {
-                result = (1..6).random()
-            }
 
-        ) {
-            Text(text = stringResource(R.string.roll))
+        Column (modifier = Modifier.fillMaxSize(1f) ,verticalArrangement = Arrangement.Bottom, horizontalAlignment = Alignment.CenterHorizontally) {
+            simpleButton()
         }
     }
 }
+
 
